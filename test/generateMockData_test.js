5a3f0f090ee788bda3d2d54930b0d5b8e792f940
mohamedrafi <fair7993@gmail.com>
2018-04-11 08:36:09 +0530

    test cases added

---
+++ test/generateMockData_test.js
@@ -0,0 +1,22 @@
+import { generateMockData } from '../generateMockData';
+
+// The module 'assert' provides assertion methods from node
+const assert = require('assert');
+
+suite('Generating mock data',()=>{
+
+  const schema = [{
+    'name': 'sam',
+    'age': 100,
+    user: [1,2]
+  }]
+
+  test('check if generate mock data works for array of objects', ()=>{
+    assert(generateMockData(schema, 5).length === 5)
+  })
+
+  test('check if generate mock data works for objects', ()=>{
+    assert(Object.keys(generateMockData(schema, 5)).length === Object.keys(schema).length)
+  })
+
+})
